87748fd5d00a1e9f32fbf88be6315f9d1b753c7f
dtanguyet <73488112+dtanguyet@users.noreply.github.com>
2021-06-29 20:55:23 +0700

    Update Profile.cs

--- ResourceDA/ResourceDA/Models/Profile.cs
+++ ResourceDA/ResourceDA/Models/Profile.cs
@@ -14,9 +14,53 @@ namespace ResourceDA.Models
     
     public partial class Profile : INotifyPropertyChanged
     {
-        public int IdAccount { get; set; }
+        string _Name;
+        int _IdAccount, _Age;
         public int Id { get; set; }
-        public string Name { get; set; }
-        public Nullable<int> Age { get; set; }
+        public string Name
+        {
+            get => _Name; 
+            set
+            {
+                if (_Name != value)
+                {
+                    _Name = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        public int IdAccount
+        {
+            get => _IdAccount; 
+            set
+            {
+                if (_IdAccount != value)
+                {
+                    _IdAccount = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        public Nullable<int> Age
+        {
+            get => _Age; 
+            set
+            {
+                if (_Age != value && value != null)
+                {
+                    _Age = (int)value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        void OnPropertyChanged([CallerMemberName] string propertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
     }
 }
+
